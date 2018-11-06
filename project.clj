711a009a7a09a9e13076635b4ba56638a64a7a80
Wennder dos Santos <wennder.santos@outlook.com>
2018-11-06 19:33:40 -0200

    Ignore ./vscode. Add clojure 1.9. Skip aot on :main

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring "1.7.0"]
                  [metosin/compojure-api "1.1.11"]]
   :uberjar-name "robots-and-dinosaurs.jar"
   :ring {:handler robotsandinosaurs.api.handler/app}
-  :main robotsandinosaurs.api.handler
+  :main ^{:skip-aot true} robotsandinosaurs.api.handler
   :profiles {:dev
              {:dependencies [[midje "1.9.2"]
                              [javax.servlet/javax.servlet-api "3.1.0"]
@@ -17,4 +17,5 @@
               :plugins [[lein-midje "3.2.1"]
                         [lein-cljfmt "0.6.0"]
                         [lein-cloverage "1.0.13"]
-                        [lein-ring "0.12.0"]]}})
+                        [lein-ring "0.12.0"]]}
+             :uberjar {:aot [robotsandinosaurs.api.handler]}})
