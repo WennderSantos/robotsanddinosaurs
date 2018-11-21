ff194a51f1c1e540606d649c6c9cbb5bcbf14e7e
Wennder dos Santos <wennder.santos@outlook.com>
2018-11-21 17:08:55 -0200

    Removed garbage

--- src/robotsandinosaurs/logic.clj
+++ src/robotsandinosaurs/logic.clj
@@ -1,27 +1,11 @@
 (ns robotsandinosaurs.logic
   (:require [clojure.string :as str]))
 
-(defmulti create-a-creature (fn [args] (args :creature)))
-
-(defmethod create-a-creature :robot [{:keys [creature current-space coord face-direction]}]
-  (assoc-in current-space [creature coord] face-direction))
-
-(defmethod create-a-creature :dinosaur [{:keys [creature current-space coord]}]
-  (update-in current-space [creature] conj coord))
-
 (defn coord-into-map [coord]
   {:x (Integer. (first (str/split coord #":"))) :y (Integer. (last (str/split coord #":")))})
 
 (defn coord-into-string [{:keys [x y]}]
   (str x ":" y))
-
-(defmulti remove-creature (fn [at] (at :creature)))
-
-(defmethod remove-creature :robot [{:keys [creature current-space coord]}]
-  (update current-space creature dissoc coord))
-
-(defmethod remove-creature :dinosaur [{:keys [creature current-space coord]}]
-  (update current-space creature disj coord))
 
 (defn- coord-x-1 [coord]
   (coord-into-string (update coord :x dec)))
