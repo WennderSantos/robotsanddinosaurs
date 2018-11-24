c1ed442b71a3034f79b1b7675877b2b04ddbd644
Wennder dos Santos <wennder.santos@outlook.com>
2018-11-24 14:16:50 -0200

    Code formating

--- src/robotsandinosaurs/logic.clj
+++ src/robotsandinosaurs/logic.clj
@@ -2,25 +2,24 @@
   (:require [clojure.string :as str]))
 
 (defn new-robot [x y face-direction id]
-  {id {:id id :coord {:x x :y y} :face-direction face-direction}})
+  {id {:id id
+       :coord {:x x :y y}
+       :face-direction face-direction}})
 
 (defn new-dinosaur [x y id]
-  {id {:id id :coord {:x x :y y}}})
+  {id {:id id
+       :coord {:x x :y y}}})
 
 (defn coord-into-string [{:keys [x y]}]
   (str x ":" y))
 
-(defn- coord-x-1 [coord]
-  (update coord :x dec))
+(defn- coord-x-1 [coord] (update coord :x dec))
 
-(defn- coord-x+1 [coord]
-  (update coord :x inc))
+(defn- coord-x+1 [coord] (update coord :x inc))
 
-(defn- coord-y-1 [coord]
-  (update coord :y dec))
+(defn- coord-y-1 [coord] (update coord :y dec))
 
-(defn- coord-y+1 [coord]
-  (update coord :y inc))
+(defn- coord-y+1 [coord] (update coord :y inc))
 
 (defn move-robot [where coord face-direction]
   (let [instruction [where face-direction]]
--- src/robotsandinosaurs/schemas.clj
+++ src/robotsandinosaurs/schemas.clj
@@ -1,13 +1,12 @@
 (ns robotsandinosaurs.schemas
   (:require [schema.core :as s]))
 
-(defonce grid {:lenght 50})
+(def ^:private grid {:lenght 50})
 
-(def Cell (s/constrained
-            s/Int
-            #(and
-              (>= % 0)
-              (< % (:lenght grid)))))
+(def ^:private Cell (s/constrained s/Int
+                                    #(and
+                                     (>= % 0)
+                                     (< % (:lenght grid)))))
 
 (def Dinosaur {:coord {:x Cell
                        :y Cell}})
