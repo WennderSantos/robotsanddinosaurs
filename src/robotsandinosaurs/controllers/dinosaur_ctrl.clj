503c6e19d3f76b3c850f3c9d65dcf322770dc0e0
Wennder dos Santos <wennder.santos@outlook.com>
2018-11-26 13:50:11 -0200

    Formatting

--- src/robotsandinosaurs/controllers/dinosaur_ctrl.clj
+++ src/robotsandinosaurs/controllers/dinosaur_ctrl.clj
@@ -4,14 +4,14 @@
             [clojure.string :as str])
   (:import [java.util UUID]))
 
-(defn create-dinosaur! [{:keys [coord]} storage]
-  (let [id (str (UUID/randomUUID))
-        dinosaur (logic/new-dinosaur (:x coord) (:y coord) id)]
-    (db.dinosaur/create! dinosaur storage)
-    {:id id}))
-
 (defn get-dinosaur [id storage]
   (db.dinosaur/get-dinosaur id storage))
 
 (defn get-dinosaurs [storage]
   (db.dinosaur/get-dinosaurs storage))
+
+(defn create-dinosaur! [{:keys [coord]} storage]
+  (let [id (str (UUID/randomUUID))
+        dinosaur (logic/new-dinosaur (:x coord) (:y coord) id)]
+    (db.dinosaur/create! dinosaur storage)
+    {:id id}))
